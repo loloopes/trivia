79afb49a0ce1e645ea27c51b0399d2c3aaa35b24
alanmdf <alanmoreirascr@gmail.com>
2021-09-02 20:56:43 -0300

    Merge requisito 5

--- src/pages/GameScreen.js
+++ src/pages/GameScreen.js
@@ -1,12 +1,70 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import BtnCorrect from '../components/BtnCorrect';
+import BtnWrong from '../components/BtnWrong';
 import Header from '../components/Header';
 
+import { getQuestionsThunk } from '../redux/actions';
+
 class GameScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.renderQuestions();
+  }
+
+  renderQuestions() {
+    const { token, fetchQuestions } = this.props;
+    fetchQuestions(token);
+    this.setState({
+      loading: false,
+    });
+  }
+
   render() {
+    const { loading } = this.state;
+    if (loading) {
+      return <span>carregando</span>;
+    }
+    const { questions } = this.props;
     return (
-      <Header />
+      <div>
+        <Header />
+        <h1 data-testid="question-category">{ questions[0].category }</h1>
+        <p data-testid="question-text">{ questions[0].question }</p>
+        <BtnCorrect correct={ questions[0].correct_answer } />
+        {questions[0].incorrect_answers.map((answer, index) => (<BtnWrong
+          key={ index }
+          wrong={ answer }
+          index={ index }
+        />))}
+      </div>
     );
   }
 }
 
-export default GameScreen;
+GameScreen.propTypes = {
+  token: PropTypes.string.isRequired,
+  fetchQuestions: PropTypes.func.isRequired,
+  questions: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  token: state.login.token,
+  questions: state.questions.questions,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchQuestions: (token) => {
+    dispatch(getQuestionsThunk(token));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameScreen);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,5 @@
 export const GET_TOKEN = 'GET_TOKEN';
-
+export const GET_QUESTIONS = 'GET_QUESTIONS';
 const getToken = (name, email, token) => ({
   type: GET_TOKEN,
   payload: {
@@ -8,6 +8,20 @@ const getToken = (name, email, token) => ({
     token,
   },
 });
+
+const getQuestions = (questions) => ({
+  type: GET_QUESTIONS,
+  payload: {
+    questions,
+  },
+});
+
+export const getQuestionsThunk = (token) => async (dispatch) => {
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+  const { results } = await response.json();
+  console.log(results);
+  dispatch(getQuestions(results));
+};
 
 const getTokenThunk = (name, email) => async (dispatch) => {
   const response = await fetch('https://opentdb.com/api_token.php?command=request');
