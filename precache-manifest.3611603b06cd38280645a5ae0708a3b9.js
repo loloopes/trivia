9fd7d83d4571c206e3c52fa1afc1b508755b8599
Lopes <gus.lcampos38@gmail.com>
2021-09-19 20:53:56 -0300

    Updates

---
+++ precache-manifest.3611603b06cd38280645a5ae0708a3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92132cbd88af33742c6a62cfbcb1a341",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "82034308e467a3cc0d85",
+    "url": "/trivia/static/css/main.93c29e5d.chunk.css"
+  },
+  {
+    "revision": "6a22985cb61f4846486a",
+    "url": "/trivia/static/js/2.42ceb002.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trivia/static/js/2.42ceb002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82034308e467a3cc0d85",
+    "url": "/trivia/static/js/main.47a45eb6.chunk.js"
+  },
+  {
+    "revision": "8cf3ddda99dc054ff6df",
+    "url": "/trivia/static/js/runtime-main.55dca74b.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
